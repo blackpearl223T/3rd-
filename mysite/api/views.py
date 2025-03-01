156234d339ab9bb03667dfc5fd808ce7ec69badd
blackpearl223T <eniolaadebimpe53@gmail.com>
2025-03-01 16:23:33 +0100

    third commit

---
+++ mysite/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+class BlogPostRetrieveUpdateDestroy(generics.BlogPostRetrieveUpdateDestroy):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
+
+
